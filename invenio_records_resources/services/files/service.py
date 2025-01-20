d3b3bf9b760a72787409c39fc2eed45c60214d4e
Mirek Simek <miroslav.simek@gmail.com>
2025-01-20 13:43:59 +0100

    feat: Pluggable transfers and multipart transfer implementation
    * Implementation of RFC 0072 - Pluggable Transfer Types for Record Files
    
    * Pluggable transfer types in invenio_records_resources/services/files/transfers
    
    * Implementation of multipart transfer in the same place
    
    * Permission generator for per-transfer-type permissions
    
    Co-authored-by: Mirek Simek <miroslav.simek@cesnet.cz>

--- invenio_records_resources/services/files/service.py
+++ invenio_records_resources/services/files/service.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2020-2021 CERN.
 # Copyright (C) 2020-2021 Northwestern University.
+# Copyright (C) 2025 CESNET.
 #
 # Invenio-Records-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -11,11 +12,13 @@
 
 from flask import current_app
 from invenio_i18n import gettext as _
+from marshmallow import ValidationError
 
 from ..base import LinksTemplate, Service
 from ..errors import FailedFileUploadException, FileKeyNotFoundError
 from ..records.schema import ServiceSchemaWrapper
 from ..uow import RecordCommitOp, unit_of_work
+from .schema import InitFileSchemaMixin
 
 
 class FileService(Service):
@@ -28,8 +31,27 @@ class FileService(Service):
 
     @property
     def file_schema(self):
-        """Returns the data schema instance."""
+        """Returns the data schema instance.
+
+        The schema can be used for dumping file's metadata or updating them.
+        For the creation of a new file, use the `initial_file_schema` property
+        as it will include the necessary fields for initiating the file upload.
+        """
         return ServiceSchemaWrapper(self, schema=self.config.file_schema)
+
+    @property
+    def initial_file_schema(self):
+        """Returns the data schema instance for initiating the file upload."""
+        if not hasattr(self.config, "initial_file_schema"):
+            self.config.initial_file_schema = type(
+                self.config.file_schema.__name__ + "Initial",
+                (
+                    InitFileSchemaMixin,
+                    self.config.file_schema,
+                ),
+                {},
+            )
+        return ServiceSchemaWrapper(self, schema=self.config.initial_file_schema)
 
     def file_result_item(self, *args, **kwargs):
         """Create a new instance of the resource unit."""
@@ -61,7 +83,9 @@ class FileService(Service):
         # FIXME: Remove "registered_only=False" since it breaks access to an
         # unpublished record.
         record = self.record_cls.pid.resolve(id_, registered_only=False)
-        self.require_permission(identity, action, record=record, file_key=file_key, **kwargs)
+        self.require_permission(
+            identity, action, record=record, file_key=file_key, **kwargs
+        )
 
         if file_key and file_key not in record.files:
             raise FileKeyNotFoundError(id_, file_key)
@@ -89,7 +113,23 @@ class FileService(Service):
     @unit_of_work()
     def init_files(self, identity, id_, data, uow=None, **kwargs):
         """Initialize the file upload for the record."""
-        record = self._get_record(id_, identity, "create_files", data=data, **kwargs)
+        # validate the input data at the beginning, as we need to check
+        # if user has permission for the transfer type that is on
+        # each of the uploaded files. This is done in the IfTransferType
+        # permission generator.
+        schema = self.initial_file_schema.schema(many=True)
+        data = schema.load(data)
+
+        if not data:
+            raise ValidationError("No files to upload.")
+
+        # resolve the record and check permissions for each uploaded file
+        record = self.record_cls.pid.resolve(id_, registered_only=False)
+
+        for created_file in data:
+            self.require_permission(
+                identity, "create_files", record=record, file_metadata=created_file
+            )
 
         self.run_components("init_files", identity, id_, record, data, uow=uow)
 
@@ -108,7 +148,9 @@ class FileService(Service):
 
         :raises FileKeyNotFoundError: If the record has no file for the ``file_key``
         """
-        record = self._get_record(id_, identity, "create_files", file_key=file_key, data=data, **kwargs)
+        record = self._get_record(
+            id_, identity, "create_files", file_key=file_key, data=data, **kwargs
+        )
 
         self.run_components(
             "update_file_metadata", identity, id_, file_key, record, data, uow=uow
@@ -127,7 +169,9 @@ class FileService(Service):
 
         :raises FileKeyNotFoundError: If the record has no file for the ``file_key``
         """
-        record = self._get_record(id_, identity, "read_files", file_key=file_key, **kwargs)
+        record = self._get_record(
+            id_, identity, "read_files", file_key=file_key, **kwargs
+        )
 
         self.run_components("read_file_metadata", identity, id_, file_key, record)
 
@@ -145,7 +189,9 @@ class FileService(Service):
 
         :raises FileKeyNotFoundError: If the record has no file for the ``file_key``
         """
-        record = self._get_record(id_, identity, "create_files", file_key=file_key, **kwargs)
+        record = self._get_record(
+            id_, identity, "create_files", file_key=file_key, **kwargs
+        )
         file_record = record.files[file_key]
 
         self.run_components(
@@ -174,7 +220,9 @@ class FileService(Service):
 
         :raises FileKeyNotFoundError: If the record has no file for the ``file_key``
         """
-        record = self._get_record(id_, identity, "commit_files", file_key=file_key, **kwargs)
+        record = self._get_record(
+            id_, identity, "commit_files", file_key=file_key, **kwargs
+        )
 
         self.run_components("commit_file", identity, id_, file_key, record, uow=uow)
 
@@ -192,7 +240,9 @@ class FileService(Service):
 
         :raises FileKeyNotFoundError: If the record has no file for the ``file_key``
         """
-        record = self._get_record(id_, identity, "delete_files", file_key=file_key, **kwargs)
+        record = self._get_record(
+            id_, identity, "delete_files", file_key=file_key, **kwargs
+        )
         deleted_file = record.files.delete(file_key, remove_rf=True)
 
         self.run_components(
@@ -241,8 +291,15 @@ class FileService(Service):
 
         :raises FileKeyNotFoundError: If the record has no file for the ``file_key``
         """
-        record = self._get_record(id_, identity, "set_content_files", file_key=file_key,
-                                  stream=stream, content_length=content_length, **kwargs)
+        record = self._get_record(
+            id_,
+            identity,
+            "set_content_files",
+            file_key=file_key,
+            stream=stream,
+            content_length=content_length,
+            **kwargs
+        )
         errors = None
         try:
             self.run_components(
@@ -278,7 +335,9 @@ class FileService(Service):
 
         :raises FileKeyNotFoundError: If the record has no file for the ``file_key``
         """
-        record = self._get_record(id_, identity, "get_content_files", file_key=file_key, **kwargs)
+        record = self._get_record(
+            id_, identity, "get_content_files", file_key=file_key, **kwargs
+        )
 
         self.run_components("get_file_content", identity, id_, file_key, record)
 
@@ -287,5 +346,75 @@ class FileService(Service):
             identity,
             record.files[file_key],
             record,
+            links_tpl=self.file_links_item_tpl(id_),
+        )
+
+    def get_transfer_metadata(self, identity, id_, file_key):
+        """Retrieve file transfer metadata."""
+        record = self._get_record(
+            id_, identity, "get_file_transfer_metadata", file_key=file_key
+        )
+        file = record.files[file_key]
+        transfer_metadata = dict(file.transfer)
+        self.run_components(
+            "get_transfer_metadata", identity, id_, file_key, record, transfer_metadata
+        )
+        return transfer_metadata
+
+    @unit_of_work()
+    def update_transfer_metadata(
+        self, identity, id_, file_key, transfer_metadata, uow=None
+    ):
+        """Update file transfer metadata."""
+        record = self._get_record(
+            id_, identity, "update_file_transfer_metadata", file_key=file_key
+        )
+        self.run_components(
+            "update_transfer_metadata",
+            identity,
+            id_,
+            file_key,
+            record,
+            transfer_metadata,
+            uow=uow,
+        )
+
+    @unit_of_work()
+    def set_multipart_file_content(
+        self, identity, id_, file_key, part, stream, content_length=None, uow=None
+    ):
+        """Save file content of a single part.
+
+        :raises FileKeyNotFoundError: If the record has no file for the ``file_key``
+        """
+        record = self._get_record(id_, identity, "set_content_files", file_key=file_key)
+        errors = None
+        try:
+            self.run_components(
+                "set_multipart_file_content",
+                identity,
+                id_,
+                file_key,
+                part,
+                stream,
+                content_length,
+                record,
+                uow=uow,
+            )
+            file = record.files[file_key]
+
+        except FailedFileUploadException as e:
+            file = e.file
+            current_app.logger.exception("File upload transfer failed.")
+            # we gracefully fail so that uow can commit the cleanup operation in
+            # FileContentComponent
+            errors = "File upload transfer failed."
+
+        return self.file_result_item(
+            self,
+            identity,
+            file,
+            record,
+            errors=errors,
             links_tpl=self.file_links_item_tpl(id_),
         )
