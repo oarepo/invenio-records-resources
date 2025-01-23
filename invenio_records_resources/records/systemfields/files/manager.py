e47146f7eafe8f6c33f300720e01eb187a0d94a0
Mirek Simek <miroslav.simek@gmail.com>
2025-01-23 14:30:17 +0100

    Files manager - metadata not lost (backported from rdm13)

--- invenio_records_resources/records/systemfields/files/manager.py
+++ invenio_records_resources/records/systemfields/files/manager.py
@@ -372,7 +372,13 @@ class FilesManager(MutableMapping):
                 if obj_or_key in self:
                     del self[obj_or_key]
             elif operation == "add":
-                self[obj_or_key.key] = obj_or_key
+                f_key = obj_or_key.key
+                self[f_key] = obj_or_key
+                rf = src_files[f_key]
+                if rf.metadata is not None:
+                    self[f_key] = obj_or_key, dict(rf)
+                else:
+                    self[f_key] = obj_or_key
 
     @property
     def entries(self):
